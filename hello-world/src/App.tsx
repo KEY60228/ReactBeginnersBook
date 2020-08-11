372724bd725f2d4e77e057016ef153ea8cec6545
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-08-11 20:57:10 +0900

    chapter.5 jsx

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -1,32 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    const logoAttrs = {
-      alt: "logo",
-      className: "App-logo",
-      src: logo
-    };
-    const title = "こんにちはReact";
-    const targets = ["World", "Kanae", "Yukina"];
+// class App extends Component {
+//   render() {
+//     const logoAttrs = {
+//       alt: "logo",
+//       className: "App-logo",
+//       src: logo
+//     };
+//     const title = "こんにちはReact";
+//     const targets = ["World", "Kanae", "Yukina"];
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          {
-            // コメント
-          }
-          <img {...logoAttrs} />
-          { title && <p>{title}</p>}
-          { targets.map(target => (
-            <p>Hello, {target}!</p>
-          ))}
-        </header>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           {
+//             // コメント
+//           }
+//           <img {...logoAttrs} />
+//           { title && <p>{title}</p>}
+//           { targets.map(target => (
+//             <p>Hello, {target}!</p>
+//           ))}
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+const Hello: FC = () => <div>Hello World!</div>;
+
+const logProps = (WrappedComponent: FC) => {
+  return class extends Component {
+    componentDidMount() {
+      console.log('Component is rendered.')
+    }
+
+    render() {
+      return <WrappedComponent />;
+    }
+  };
+};
+
+// export default App;
+export default logProps(Hello);
